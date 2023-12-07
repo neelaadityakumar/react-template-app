a298dcb382b9557d9934402b15063cdad0653bc0
Adity Kumar <neelaadityakumar@gmail.com>
2023-12-07 15:25:03 +0530

    add

---
+++ src/component/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+    const navigate=useNavigate()
+  return (
+    <div>route
+
+        <button onClick={()=>navigate("/list", { state: { id: 123, category: 'example' } })}>change</button>
+    </div>
+  )
+}
+
+export default Home
