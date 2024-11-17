f89dd2d7e8bd0704605332378197512299152831
Aditya Kumar <neelaadityakumar@gmail.com>
2024-11-17 22:09:58 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Home from './component/Home';
-import List from './component/List';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./component/Home";
+import List from "./component/List";
+import Virtualization from "./component/Virtualization";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/list" element={<List />} />
+          <Route path="/virtual" element={<Virtualization />} />
 
           {/* Other routes */}
         </Routes>
