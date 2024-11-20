eda4338c24d4fbbd16e54bedca406c3ac5c93f8e
Aditya Kumar <neelaadityakumar@gmail.com>
2024-11-20 20:39:47 +0530

    add toast

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./component/Home";
 import List from "./component/List";
 import Virtualization from "./component/Virtualization";
+import Toast from "./component/Toast";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/list" element={<List />} />
           <Route path="/virtual" element={<Virtualization />} />
+          <Route path="/toast" element={<Toast />} />
 
           {/* Other routes */}
         </Routes>
